510b1c1be43deebfce56d2e767231c33e30d7b0b
muratkeremozcan <muratkerem@gmail.com>
2025-01-07 07:16:45 -0600

    test update

--- pw/download-multiple.spec.ts
+++ pw/download-multiple.spec.ts
@@ -1,13 +1,10 @@
 import { test, expect } from '@playwright/test'
-import isCi from 'is-ci'
 import fs from 'node:fs'
 
 test('Download Multiple files', async ({ page }) => {
-  if (isCi) test.skip()
-
   await page.goto('https://the-internet.herokuapp.com/download')
 
-  const downloadFiles = ['TestData.xlsx', 'dummy.txt']
+  const downloadFiles = ['LambdaTest.txt', 'black.jpg']
 
   for (const fileName of downloadFiles) {
     const [download] = await Promise.all([
