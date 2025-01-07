bb31e3134cda98beaa4d1bb8991a1f4c20060231
muratkeremozcan <muratkerem@gmail.com>
2025-01-07 07:24:57 -0600

    best settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 2,
+  retries: process.env.CI ? 5 : 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'html' : 'list',
 
   timeout: 15000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
