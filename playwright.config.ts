957fdf4487e38db4e4bda00fee89f5aa839d6d90
muratkeremozcan <muratkerem@gmail.com>
2025-01-07 08:48:15 -0600

    readme

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: process.env.PW_REPORTER || (process.env.CI ? 'html' : 'list'),
-  reporter: 'html',
+  reporter: process.env.CI ? 'html' : 'list',
 
   timeout: 15000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
