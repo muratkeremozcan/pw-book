e2cc4c848f23867a0add855e76a9418e2c223e81
muratkeremozcan <muratkerem@gmail.com>
2025-01-09 12:14:59 -0600

    check sub section

--- pw/visual-testing/visual-testing.spec.ts
+++ pw/visual-testing/visual-testing.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test'
 
-test.beforeEach(({ page }) => page.goto('https://playwright.dev/'))
+// Restrict to Chrome only
+test.use({ browserName: 'chromium' })
 
 // the options are optional and nice for customization
 test('Take visual of full page with masking the button', async ({ page }) => {
-  await expect(page).toHaveScreenshot('visual-mask.png', {
-    fullPage: true,
+  await page.goto('https://playwright.dev/')
+
+  const element = page.locator('.hero__title') // Targeting subsection of the page
+
+  // we can check th whole page, but it becomes a headache in CI
+  await expect(element).toHaveScreenshot('visual-subsection.png', {
+    // fullPage: true,
     mask: [page.getByText('Get started')],
     maxDiffPixels: 400
   })
